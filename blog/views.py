5c318a4ab829c742824421486118f8f5991bc805
Dennis Peterson <denispeterson96@gmail.com>
2019-04-01 11:28:10 +0300

    Removed bootstrap

--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            # post.published_date = timezone.now()
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
